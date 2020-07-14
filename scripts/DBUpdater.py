6799b12f6e32fd6fd320816363407461caa7d21c
skylar <qvalador@gmail.com>
2020-07-14 17:09:44 -0400

    Add command-line tool for DBUpdater.py

--- scripts/DBUpdater.py
+++ scripts/DBUpdater.py
@@ -1,6 +1,7 @@
 import os
 import json
 import ScrapingTools
+from sys import argv
 from pymongo import MongoClient
 
 class DBUpdater:
@@ -25,12 +26,12 @@ class DBUpdater:
         """
         if content_type not in self.scrapers.keys():
             raise ValueError("Invalid content type.  Choose one of [dex, moves, abilities]")
-        data = self.scrapers[content_type].get_data(name)
         filter_ = {"name": name}
         existing_record = self.db[content_type].find_one(filter_)
         was_updated = False
         if existing_record:
             print("Record for {} exists (id={})".format(name, existing_record["_id"]))
+            data = self.scrapers[content_type].get_data(name)
             needs_updating = {}
             for field in data:
                 # add non-existent fields or update inaccurate ones
@@ -56,6 +57,7 @@ class DBUpdater:
         else:
             # create record if necessary
             print("No existing record for {}.  Creating one now...".format(name), flush=True)
+            data = self.scrapers[content_type].get_data(name)
             record = self.db[content_type].insert_one(data)
             print("Record for {} created successfully (id={})".format(name, record.inserted_id))
 
@@ -75,5 +77,27 @@ class DBUpdater:
 
 if __name__ == "__main__":
     updater = DBUpdater()
-    updater.update_type("moves")
-    updater.update_type("abilities")
+    valid_content_types = {"pokemon": "dex", "move": "moves", "ability": "abilities", "all": ''}
+    if len(argv) == 1:
+        content_type = ""
+        while content_type.lower() not in valid_content_types:
+            content_type = input("What type of content would you like to update? [{}]\n> ".format("/".join([*valid_content_types.keys()])))
+            if content_type not in valid_content_types:
+                print("Invalid content type.  Choose one of [{}]\n> ".format("/".join([*valid_content_types.keys()])))
+    else:
+        content_type = argv[1]
+        if content_type not in valid_content_types:
+            raise ValueError("Invalid content type.  Choose one of [{}]\n> ".format("/".join([*valid_content_types.keys()])))
+    if content_type == "all":
+        updater.update_all()
+    else:
+        if len(argv) == 3:
+            record = argv[2].split(",")
+        else:
+            print("What record will you update?  You may enter a comma-separated list of record names (no spaces), or 'all' to update all {} records.".format(content_type))
+            record = input("> ").split(",")
+        if record[0] == "all":
+            updater.update_type(valid_content_types[content_type])
+        else:
+            for item in record:
+                updater.update(item, valid_content_types[content_type])
